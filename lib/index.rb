536ca98de3ee3060b057934a9bd6e1ec4d993af7
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-01-31 10:46:23 +0100

    Require indexers.

--- lib/index.rb
+++ lib/index.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 require 'chewy'
-require 'index/version'
+require 'index/organization_index'
 require 'index/repository_index'
+require 'index/user_index'
+require 'index/version'
